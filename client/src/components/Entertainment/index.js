7f1cce254676005c00693824c9780b020c10ad5a
satinder <satinder042890@gmail.com>
2019-04-29 13:46:01 -0700

    news displayed on entertainment page

--- client/src/components/Entertainment/index.js
+++ client/src/components/Entertainment/index.js
@@ -1,27 +1,57 @@
-import React, {Component} from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Header from "./Header/Header.js";
-import Musix from "./Musix.js";
-import Lyrics from './Lyrics/Lyrics.js';
-import { Provider } from './Context.js';
-
-
+import React, { Component } from "react";
+import Navbar from "../Navbar";
+import API from "../../utils/DbAPI";
+import Form from "../Form/Formgroup";
+import "./style.css";
+import * as moment from 'moment';
 class Entertainment extends Component {
+    state={
+        result:[]
+    }
+    showNews=()=>{
+        API.getNews()
+        .then(res => {
+            console.log(res.data.articles)
+            this.setState({result:res.data.articles})
+           
+            console.log(this.state.result);
+            console.log("hello");
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
     render() {
         return (
-            <Provider>
-                <Router>
-                    <React.Fragment>
-                        <div className="container">
-                            <Switch>
-                                <Route path="/entertainment" component={Musix} />
-                                <Route path="/lyrics/track/:id" component={Lyrics} />
-                            </Switch>
-                        </div>
-                    </React.Fragment>
-                </Router>
-            </Provider>
-      )
+            <div>
+                <Navbar id={this.props.match.params.id} />
+                <ul className="list-inline text-center list">
+                    <li className="list-inline-item first" onClick={this.showNews}>News</li>
+                    <li className="list-inline-item">Games</li>
+                    <li className="list-inline-item" >Parties</li>
+                </ul>
+               <div id="news">
+                {this.state.result.map(news =>
+                <div className="container">
+                <div className="row">
+                   <a href={news.url} id="link" target="_blank"><h5>{news.title}</h5> </a>
+                </div>
+                <div className="row">
+                <div className="col-3">
+                  <img src={news.urlToImage} id="newsimg"/>
+                </div>
+                <div className="col-9">
+                <p>{news.description}</p>
+                <h6>Published At : {moment(news.publishedAt).format("dddd, MMMM Do YYYY, h:mm:ss a")} </h6>
+                </div>
+            </div>
+            <hr></hr>
+               </div>
+                  )} 
+                </div>
+                  
+            </div>
+        )
     }
 }
 
